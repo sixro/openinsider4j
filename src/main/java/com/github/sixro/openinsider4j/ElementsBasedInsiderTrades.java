87de7d27de21f5155b15f4f816eb26c64ca814d8
sixro <me@sixro.net>
2021-07-09 19:18:50 +0200

    fix: used GoogleFinanceTickerResolver by default

--- src/main/java/com/github/sixro/openinsider4j/ElementsBasedInsiderTrades.java
+++ src/main/java/com/github/sixro/openinsider4j/ElementsBasedInsiderTrades.java
@@ -3,25 +3,31 @@ package com.github.sixro.openinsider4j;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.Iterator;
+
 class ElementsBasedInsiderTrades implements InsiderTrades {
 
     private final Elements elements;
+    private final TickerResolver tickerResolver;
 
-    ElementsBasedInsiderTrades(Elements elements) {
+    ElementsBasedInsiderTrades(Elements elements, TickerResolver tickerResolver) {
 
         this.elements = elements;
+        this.tickerResolver = tickerResolver;
     }
 
     @Override
     public Iterator<InsiderTrade> iterator() {
-        return new ElementsBasedInsiderTrades.Iterator<>(this.elements.iterator());
+        return new ElementsBasedInsiderTrades.Iterator<>(this.elements.iterator(), tickerResolver);
     }
 
     private static class Iterator<I> implements java.util.Iterator<InsiderTrade> {
         private final java.util.Iterator<Element> it;
+        private final TickerResolver tickerResolver;
 
-        Iterator(java.util.Iterator<Element> it) {
+        Iterator(java.util.Iterator<Element> it, TickerResolver tickerResolver) {
             this.it = it;
+            this.tickerResolver = tickerResolver;
         }
 
         @Override
@@ -32,7 +38,7 @@ class ElementsBasedInsiderTrades implements InsiderTrades {
         @Override
         public InsiderTrade next() {
             Element el = it.next();
-            return new ElementBasedInsiderTrade(el, null);
+            return new ElementBasedInsiderTrade(el, tickerResolver);
         }
 
     }
